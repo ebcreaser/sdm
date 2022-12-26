b3cc45fe2286eedba08e556c24df5a44326e0a15
Ethan Creaser <ebcreaser@proton.me>
2022-12-26 09:50:26 -0500

    first commit

---
+++ Makefile
@@ -0,0 +1,2 @@
+sdm: sdm.c
+	gcc -Wall -Werror -pedantic -g -lcrypt sdm.c -o sdm
---
+++ sdm.c
@@ -0,0 +1,122 @@
+#include <crypt.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <pwd.h>
+#include <shadow.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define MAXPW 256
+
+static int getty(char *path);
+static struct passwd *getpw();
+
+static int
+getty(char *path)
+{
+	int fd;
+
+	if ((fd = open(path, O_RDWR)) < 0) {
+		return -1;
+	}
+	if ((fd = open(path, O_RDWR)) < 0) {
+		return -1;
+	}
+	if (dup2(fd, STDOUT_FILENO) != STDOUT_FILENO) {
+		return -1;
+	}
+	if (dup2(fd, STDIN_FILENO) != STDIN_FILENO) {
+		return -1;
+	}
+	if (dup2(fd, STDERR_FILENO) != STDERR_FILENO) {
+		return -1;
+	}
+
+	return fd;
+}
+
+static struct passwd *
+getpw()
+{
+	struct spwd *sp;
+	char user[256];
+	char passwd[256];
+	char *hash;
+	int c, i;
+
+	fputs("Username: ", stdout);
+	i = 0;
+	while((c = getchar()) != '\n' && c != EOF && i < MAXPW - 1) {
+		user[i++] = c;
+	}
+	user[i] = '\0';
+	fputs("Password: ", stdout);
+	i = 0;
+	while((c = getchar()) != '\n' && c != EOF && i < MAXPW - 1) {
+		passwd[i++] = c;
+	}
+	passwd[i] = '\0';
+	if ((sp = getspnam(user)) == NULL) {
+		return NULL;
+	}
+	hash = crypt(passwd, sp->sp_pwdp);
+	if (strcmp(hash, sp->sp_pwdp)) {
+		fputs("Incorrect\n", stdout);
+		return NULL;
+	} else {
+		fputs("Correct\n", stdout);
+		return getpwnam(user);
+	}
+}
+
+int
+main(int argc, char *argv[])
+{
+	struct passwd *pw;
+	char vtarg[4] = "vt";
+	int fd;
+	pid_t pid;
+
+	if (argc < 2) {
+		goto error;
+	}
+	if (strstr(argv[1], "/dev/tty") != argv[1] || !isdigit(argv[1][8])) {
+		goto error;
+	}
+	vtarg[2] = argv[1][8];
+	pid = fork();
+	if (pid > 0) {
+		exit(EXIT_SUCCESS);
+	} else if (pid < 0) {
+		goto error;
+	}
+	if (setsid() < 0) {
+		goto error;
+	}
+	if ((fd = getty(argv[1])) < 0) {
+		goto error;
+	}
+	while(!(pw = getpw()));
+	if (!pw) {
+		goto error;
+	}
+	if ((pid = fork()) == 0) {
+		execl("/bin/Xorg", "/bin/Xorg", ":1", vtarg, (char *) NULL);
+		goto error;
+	} else if (pid < 0) {
+		goto error;
+	}
+	sleep(5);
+	setuid(pw->pw_uid);
+	setenv("SHELL", pw->pw_shell, 1);
+	setenv("HOME", pw->pw_dir, 1);
+	setenv("DISPLAY", ":1", 1);
+	chdir(pw->pw_dir);
+	execl(pw->pw_shell, pw->pw_shell, "--login", "-c", "dwm", (char *) NULL);
+	exit(EXIT_SUCCESS);
+error:
+	exit(EXIT_FAILURE);
+}

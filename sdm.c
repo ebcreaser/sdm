f23a964c593db114288b44dab551adf31f6c2b0a
Ethan Creaser <ebcreaser@proton.me>
2023-01-07 11:52:08 -0500

    refactored

--- sdm.c
+++ sdm.c
@@ -14,26 +14,50 @@
 
 #define SDM_MAXPW 256
 
-static void clear();
-static int getty(char *path);
-static char *gethash(struct passwd *pw);
-static struct passwd *getpw();
-static int runsession(struct passwd *pw, char *vtarg);
+static struct passwd *user_pw;
+static char xinitcmd[256] = 
+"/usr/bin/xinit ./.xinitrc -- /etc/X11/xinit/xserverrc ";
+static char tty[11] = "/dev/tty\0\0";
+static char vt[5] = "vt\0\0";
+static char display[4] = ":1\0";
+static int fd;
 
-static void
-clear()
-{
-	system("/bin/clear");
+static int  handleargs(int argc, char *argv[]);
+static int  getty();
+static int  getuser();
+static void runX();
+
+static int handleargs(int argc, char *argv[]) {
+	const struct option vtopt = {
+		.name = "vt",
+		.has_arg = 1,
+		.flag = NULL,
+		.val = 'v'
+	};
+	const char *optstring = "v:";
+	int i;
+
+	switch (getopt_long(argc, argv, optstring, &vtopt, NULL)) {
+		case 'v':
+			for (i = 0; optarg[i]; ++i) {
+				if (!isdigit(optarg[i])) {
+					return -1;
+				}
+			}
+			strncpy(tty + 8, optarg, 2);
+			strncpy(vt + 2, optarg, 2);
+			return 0;
+		default:
+			return -1;
+	}
 }
 
 /* open tty device at path and set stdin, stdout, and stderr
  */
 static int
-getty(char *path)
+getty()
 {
-	int fd;
-
-	if ((fd = open(path, O_RDWR)) < 0) {
+	if ((fd = open(tty, O_RDWR)) < 0) {
 		return -1;
 	}
 	if (dup2(fd, STDOUT_FILENO) != STDOUT_FILENO) {
@@ -46,39 +70,22 @@ getty(char *path)
 		return -1;
 	}
 
-	return fd;
-}
-
-static char *
-gethash(struct passwd *pw)
-{
-	struct spwd *sp;
-	char *hash;
-
-	hash = pw->pw_passwd;
-	if (!strcmp(hash, "x")) {
-		if ((sp = getspnam(pw->pw_name)) == NULL) {
-			return NULL;
-		}
-		hash = sp->sp_pwdp;
-	}
-
-	return hash;
+	return 0;
 }
 
 /* prompt user for username and password, and authenticate against shadow file
  */
-static struct passwd *
-getpw()
+static int 
+getuser()
 {
-	struct passwd *pw;
+	struct spwd *sp;
 	struct termios term;
 	char user[SDM_MAXPW];
-	char passwd[SDM_MAXPW];
-	char *hash, *inputhash;
+	char password[SDM_MAXPW];
+	char *hash;
 	int c, i;
 
-	clear();
+	system("/usr/bin/clear");
 	fputs("Username: ", stdout);
 	i = 0;
 	while((c = getchar()) != '\n' && c != EOF && i < SDM_MAXPW - 1) {
@@ -92,77 +99,63 @@ getpw()
 	term.c_lflag &= ~ECHO;
 	tcsetattr(STDIN_FILENO, 0, &term);
 	while((c = getchar()) != '\n' && c != EOF && i < SDM_MAXPW - 1) {
-		passwd[i++] = c;
+		password[i++] = c;
 	}
-	passwd[i] = '\0';
+	password[i] = '\0';
 	/* reenable input echo */
 	term.c_lflag |= ECHO;
 	tcsetattr(STDIN_FILENO, 0, &term);
-	/* hash inputed password and compare against shadow file entry */
-	if ((pw = getpwnam(user)) == NULL) {
-		return NULL;
+	if ((user_pw = getpwnam(user)) == NULL) {
+		return -1;
 	}
-	hash = gethash(pw);
-	inputhash = crypt(passwd, hash);
-	if (strcmp(inputhash, hash)) {
+	/* hash input with salt from stored hash */
+	hash = user_pw->pw_passwd;
+	if (!strcmp(hash, "x")) {
+		if ((sp = getspnam(user_pw->pw_name)) == NULL) {
+			return -1;
+		}
+		hash = sp->sp_pwdp;
+	}
+	if (strcmp(hash, crypt(password, hash))) {
 		fputs("\nIncorrect\n", stdout);
-		return NULL;
+		return -1;
 	} else {
 		fputs("\nCorrect\n", stdout);
-		return pw;
+		return 0;
 	}
 }
 
 /* set env variables and run X
  */
-static int
-runsession(struct passwd *pw, char *vtarg) {
-	pid_t pid;
-
-	setuid(pw->pw_uid);
-	initgroups(pw->pw_name, pw->pw_gid);
-	chdir(pw->pw_dir);
-	setenv("HOME", pw->pw_dir, 1);
-	setenv("SHELL", pw->pw_shell, 1);
-	if ((pid = fork()) == 0) {
-		execl(pw->pw_shell, pw->pw_shell, "--login", "-c", "/usr/bin/startx", (char *) NULL);
-		return -1;
-	} else if (pid < 0) {
-		return -1;
-	}
-	wait(NULL);
-	exit(EXIT_SUCCESS);
+static void
+runX() {
+	strcat(xinitcmd, display);
+	strcat(xinitcmd, " ");
+	strcat(xinitcmd, vt);
+	strcat(xinitcmd, " -keeptty");
+	setuid(user_pw->pw_uid);
+	setgid(user_pw->pw_gid);
+	initgroups(user_pw->pw_name, user_pw->pw_gid);
+	chdir(user_pw->pw_dir);
+	setenv("HOME", user_pw->pw_dir, 1);
+	setenv("SHELL", user_pw->pw_shell, 1);
+	setenv("DISPLAY", display, 1);
+	execl(
+			user_pw->pw_shell, user_pw->pw_shell,
+			"--login", "-c", xinitcmd,
+			(char *) NULL
+		 );
 }
 
 int
 main(int argc, char *argv[])
 {
-	const struct option vtopt = {
-		.name = "vt",
-		.has_arg = 1,
-		.flag = NULL,
-		.val = 'v'
-	};
-	const char *optstring = "v:";
-	struct passwd *pw;
-	char ttypath[] = "/dev/tty7";
-	char vtarg[] = "vt7";
-	int fd, status;
 	pid_t pid;
 
-	switch (getopt_long(argc, argv, optstring, &vtopt, NULL)) {
-		case 'v':
-			if (isdigit(*optarg)) {
-				ttypath[8] = *optarg;
-				vtarg[2] = *optarg;
-				break;
-			}
-		default:
-			printf("Usage: %s --vt|-v [TTY NUMBER]\n", argv[0]);
-			goto error;
+	if (handleargs(argc, argv) < 0) {
+		printf("Usage: %s --vt|-v [TTY NUMBER]\n", argv[0]);
 	}
-	pid = fork();
-	if (pid > 0) {
+	if ((pid = fork()) > 0) {
 		exit(EXIT_SUCCESS);
 	} else if (pid < 0) {
 		goto error;
@@ -170,25 +163,17 @@ main(int argc, char *argv[])
 	if (setsid() < 0 || setgid(5) < 0) {
 		goto error;
 	}
-	if ((fd = getty(ttypath)) < 0) {
+	if (getty() < 0) {
 		goto error;
 	}
 	while (1) {
-		while(!(pw = getpw()));
-		if (!pw) {
-			goto error;
-		}
-		chown(ttypath, pw->pw_uid, pw->pw_gid);
+		while (getuser() < 0);
 		if ((pid = fork()) == 0) {
-			runsession(pw, vtarg);
+			runX();
 		} else if (pid < 0) {
-			chown(ttypath, 0, 5);
 			goto error;
 		}
-		waitpid(pid, &status, 0);
-		chown(ttypath, 0, 5);
-		sleep(2);
-		continue;
+		wait(NULL);
 	}
 error:
 	close(fd);
